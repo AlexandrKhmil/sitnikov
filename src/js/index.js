0eddc30e64afdde9b905c26b339698572b685476
AlexandrKhmil <oleksandr.khmil2@gmail.com>
2019-10-27 20:46:04 +0200

    added pages

---
+++ src/js/index.js
@@ -0,0 +1,3 @@
+import {lab1Canvas} from './lab1/canvas.js'
+
+lab1Canvas()
---
+++ src/js/lab1/canvas.js
@@ -0,0 +1,6 @@
+export let lab1Canvas = () => {
+  let canvas = document.querySelector('#lab1Canvas')
+  let ctx = canvas.getContext('2d')
+  
+  console.log(canvas,ctx)
+}
